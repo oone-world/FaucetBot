f31d01a2f9e4f0d6c51998c22a4efcd4f035ddc6
Pavel Solovyev <pavel.soloviev@latoken.com>
2023-08-24 14:23:03 +0300

    Changed to Buttons

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,8 @@
-const { activityType, activityName } = require('../config.json');
+const { activityType, activityName, channelId } = require('../config.json');
+const {
+	MessageActionRow,
+	MessageButton,
+} = require('discord.js');
 
 module.exports = {
 	name: 'ready',
@@ -6,5 +10,17 @@ module.exports = {
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		client.user.setActivity(activityName, { type: activityType });
+
+		const button = new MessageActionRow();
+		button.addComponents(
+			new MessageButton()
+				.setCustomId('faucet-button')
+				.setStyle('PRIMARY')
+				.setLabel('Faucet'),
+		);
+		client.channels.cache.get(channelId).send({
+			content: 'Click on Faucet to get tOONE',
+			components: [button],
+		});
 	},
 };
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const { Client, Collection, Intents } = require('discord.js');
-const { token, cooldown, approvedRoles } = require('./config.json');
+const { Client, Intents, Modal, TextInputComponent, MessageActionRow, MessageEmbed } = require('discord.js');
+const { token, cooldown, approvedRoles, amount } = require('./config.json');
 const fs = require('fs');
 const isAddress = require('./utils/address');
+const sendFunds = require('./utils/sendFunds.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const Keyv = require('keyv');
 const keyv = new Keyv();
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
 
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
@@ -26,41 +20,63 @@ for (const file of eventFiles) {
 }
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+	if (interaction.isButton()) {
+		if (interaction.customId === 'faucet-button') {
+			const modal = new Modal()
+				.setCustomId('faucet-modal')
+				.setTitle('Faucet')
+				.addComponents([
+					new MessageActionRow().addComponents(
+						new TextInputComponent()
+							.setCustomId('faucet-input')
+							.setLabel('Address')
+							.setStyle('SHORT')
+							.setMinLength(42)
+							.setMaxLength(42)
+							.setPlaceholder('0x0000000000000000000000000000000000000000')
+							.setRequired(true),
+					),
+				]);
 
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	// Rate limiting and cooldowns for faucet requests
-	if (command.data.name === 'faucet') {
-		const address = interaction.options.get('address').value.trim();
-
-		if (!isAddress(address)) {
-			return interaction.reply('Please enter a valid Ethereum Address');
-		}
-
-		// If the last transaction was less than 15 seconds ago, disallow to prevent nonce reuse (no concurrent transactions ATM)
-		const lastTx = await keyv.get('lastTx');
-		if (lastTx > Date.now() - 15000) {
-			const timeLeft = 15000 - (Date.now() - lastTx);
-			return interaction.reply(`Please wait 15 seconds between requests to prevent nonce issues. Try again in ${timeLeft / 1000}s.`);
-		}
-
-		if (!approvedRoles.some(role => interaction.member.roles.cache.has(role))) {
-			const lastRequested = await keyv.get(interaction.user.id);
-			if (lastRequested) {
-				if (Date.now() - lastRequested < cooldown) {
-					const timeLeft = Math.floor(((cooldown - (Date.now() - lastRequested)) / 1000) / 60);
-					return interaction.reply(`You can only request funds once every 60 minutes. Please try again in ${timeLeft} minutes.`);
-				}
-			}
+			await interaction.showModal(modal);
 		}
 	}
 
-	try {
-		await command.execute(interaction);
-		if (command.data.name === 'faucet') {
+	if (interaction.isModalSubmit()) {
+		if (interaction.customId === 'faucet-modal') {
+			const address = interaction.fields.getTextInputValue('faucet-input');
+			if (!isAddress(address)) {
+				return interaction.reply({ content: 'Please enter a valid Ethereum Address', ephemeral: true });
+			}
+
+			// If the last transaction was less than 15 seconds ago, disallow to prevent nonce reuse (no concurrent transactions ATM)
+			const lastTx = await keyv.get('lastTx');
+			if (lastTx > Date.now() - 15000) {
+				const timeLeft = 15000 - (Date.now() - lastTx);
+				return interaction.reply({ content: `Please wait 15 seconds between requests to prevent nonce issues. Try again in ${timeLeft / 1000}s.`, ephemeral: true });
+			}
+
+			if (!approvedRoles.some(role => interaction.member.roles.cache.has(role))) {
+				const lastRequested = await keyv.get(interaction.user.id);
+				if (lastRequested) {
+					if (Date.now() - lastRequested < cooldown) {
+						const timeLeft = Math.floor(((cooldown - (Date.now() - lastRequested)) / 1000) / 60);
+						return interaction.reply({ content: `You can only request funds once every 60 minutes. Please try again in ${timeLeft} minutes.`, ephemeral: true });
+					}
+				}
+			}
+
+			const request = await sendFunds(address, amount);
+			if (request.status === 'success') {
+				const embed = new MessageEmbed()
+					.setColor('#3BA55C')
+					.setDescription(`[View on Oonescan](https://dev.oonescan.com/tx/${request.message})`);
+				interaction.reply({ content: `Transaction for ${amount} tOONE created.`, embeds: [embed], ephemeral: true });
+			}
+			else {
+				interaction.reply({ content: `Failed to send funds. Error: ${request.message}`, ephemeral: true });
+			}
+
 			// If not an approved role, set the last requested time
 			if (!approvedRoles.some(role => interaction.member.roles.cache.has(role))) {
 				await keyv.set(interaction.user.id, Date.now());
@@ -68,20 +84,6 @@ client.on('interactionCreate', async interaction => {
 			await keyv.set('lastTx', Date.now());
 		}
 	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
 });
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
 
 client.login(token);
